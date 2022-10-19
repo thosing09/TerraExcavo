6c400cab0f8267911f5d5c66bee1271da6501214
thosing09 <93552193+thosing09@users.noreply.github.com>
2022-10-19 17:55:07 +0100

    Add files via upload

---
+++ Game1.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace compsci_NEA
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private entity _character;
+
+        private map _map;
+
+        private Color _saulColor;
+        private Texture2D _ballMan;
+        private Rectangle _ballManRect;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            _map = new map(Content.Load<Texture2D>("ballsoodman"), Content.Load<Texture2D>("stone"));
+            _character = new entity(new Rectangle(50, 100, 50, 75), Content.Load<Texture2D>("mario"), 800);
+
+            _saulColor = Color.White;
+
+            _ballManRect = new Rectangle(20,20, 15,15);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            _ballMan = Content.Load<Texture2D>("ballsoodman");
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                _ballManRect.Y--;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                _ballManRect.Y++;
+
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                _ballManRect.X--;
+
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                _ballManRect.X++;
+
+            }
+            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+            {
+                _map.Kill(_ballManRect);
+            }
+
+            if (_map.IsColliding(_ballManRect))
+            {
+                _saulColor = Color.Red;
+            }
+            else
+            {
+                _saulColor = Color.White;
+            }
+
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin(samplerState: SamplerState.PointWrap);           
+            _character.Draw(_spriteBatch, Color.White);
+            _map.Draw(_spriteBatch);
+
+            _spriteBatch.Draw(_ballMan, _ballManRect, _saulColor);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ map.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace compsci_NEA
+{
+    class map
+    {
+        private OpenSimplexNoise _accessSimplex;
+        private Texture2D _background;
+
+        const int _blockSize = 25;
+        const int _mapWidth = 60;
+        const int _mapHeight = 60;
+
+        Texture2D _stone;
+        private blockType[] _map;
+
+        enum blockType
+        {
+            sky,
+            stone,
+            dirt,
+        };
+
+        public map(Texture2D background, Texture2D stone)
+        {
+            // TODO: Add your initialization logic here
+
+            _background = background;
+            _stone = stone;
+
+            _accessSimplex = new OpenSimplexNoise();
+
+            float scale = 3f;
+            float noiseValue;
+            _map = new blockType[_mapWidth * _mapHeight];
+
+            for (int x = 0; x < _mapWidth; x++)
+            {
+                for (int y = 0; y < _mapHeight; y++)
+                {
+                    //get noise value at scale
+                    noiseValue = (float)_accessSimplex.Evaluate(x / scale, y / scale);
+
+                    if (noiseValue < 0.12)
+                    {
+                        _map[(y * _mapWidth) + x] = blockType.stone;
+                    }
+                    else
+                    {
+                        _map[(y * _mapWidth) + x] = blockType.sky;
+                    }
+                }
+            }
+        }
+
+        public void Draw(SpriteBatch _spriteBatch)
+        {
+            _spriteBatch.Draw(_background, new Rectangle(0, 0, 800, 800), Color.Red);
+
+            for (int i = 0; i < _map.Length; i++)
+            {
+                int y = (i / _mapWidth) * _blockSize;
+                int x = (i % _mapWidth) * _blockSize;
+
+                if (_map[i] == blockType.stone)
+                {
+                    _spriteBatch.Draw(_stone, new Rectangle(x, y, _blockSize, _blockSize), Color.White);
+                }
+            }
+        }
+
+        public bool IsColliding(Rectangle rect)
+        {
+            for (int x = rect.X; x < rect.X + rect.Width; x++)
+            {
+                for (int y = rect.Y; y < rect.Y + rect.Height; y++)
+                {
+                    int currentBlock = ((y / _blockSize) * _mapWidth) + (x / _blockSize);
+                    if (_map[currentBlock] != blockType.sky)
+                    {
+                        if (new Rectangle(x, y, _blockSize, _blockSize).Intersects(rect))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        //DELETE THIS PLEASE IT IS BETA DELETE THIS PLEASE IT IS BETA DELETE THIS PLEASE IT IS BETA DELETE THIS PLEASE IT IS BETA DELETE THIS PLEASE IT IS BETA
+        public bool Kill(Rectangle rect)
+        {
+            for (int i = 0; i < _map.Length; i++)
+            {
+                int y = (i / _mapWidth) * _blockSize;
+                int x = (i % _mapWidth) * _blockSize;
+
+                if (_map[i] != blockType.sky)
+                {
+                    if (new Rectangle(x, y, _blockSize, _blockSize).Intersects(rect))
+                    {
+                        _map[i] = blockType.sky;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
